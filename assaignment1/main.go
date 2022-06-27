e3ff1bc2d016e178d8a2df5552cddec6caf2acfe
Nathor003 <106574024+Nathor003@users.noreply.github.com>
2022-06-27 11:45:41 +0700

    Update main.go

--- assaignment1/main.go
+++ assaignment1/main.go
@@ -16,15 +16,20 @@ type dataOrang struct {
 func main() {
 
 	index := os.Args[1]
-	data := make([]dataOrang, 3)
 
-	fmt.Println("index", index)
-
-	data[0] = dataOrang{name: "andi", alamat: "jakarta", pekerjaan: "sales", alasan: "Penasaran aja"}
-	data[1] = dataOrang{name: "ando", alamat: "jakarta", pekerjaan: "programmer", alasan: "butuh kerja"}
-	data[2] = dataOrang{name: "budi", alamat: "jakarta", pekerjaan: "telemarketing", alasan: "pindah haluan"}
+	person := []dataOrang{
+		{
+			name: "andi", alamat: "jakarta", pekerjaan: "sales", alasan: "Penasaran aja",
+		},
+		{
+			name: "ando", alamat: "jakarta", pekerjaan: "programmer", alasan: "butuh kerja",
+		},
+		{
+			name: "budi", alamat: "jakarta", pekerjaan: "telemarketing", alasan: "pindah haluan",
+		},
+	}
 
 	idx, _ := strconv.Atoi(index)
 
-	fmt.Println(data[idx])
+	fmt.Println(person[idx])
 }
