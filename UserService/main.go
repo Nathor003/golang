8122dfc656d1d33832bc2329a7c0d28f3692e563
Nathor003 <106574024+Nathor003@users.noreply.github.com>
2022-06-29 08:07:09 +0700

    Update main.go

--- UserService/main.go
+++ UserService/main.go
@@ -16,15 +16,4 @@ func main() {
 		Age:      9,
 	})
 
-	// if user, err := userSvc.Register(&entity.User{
-	// 	Username: "budi123",
-	// 	Email:    "budi123@gmail.com",
-	// 	Password: "password123",
-	// 	Age:      9,
-	// }); err != nil {
-	// 	fmt.Printf("Error when register user: %+v", err)
-	// 	return
-	// } else {
-	// 	fmt.Printf("Success register user: %+v", user)
-	// }
 }
